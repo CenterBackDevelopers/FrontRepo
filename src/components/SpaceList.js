617c998be2f9f1e72c337f1bd23eb42b10b2f10c
JunWatson <kjoon418@naver.com>
2024-07-29 21:26:40 +0900

    Fix css bug
    SpaceList.js and TodoList.js had bug that color of border is always
    white

--- src/components/SpaceList.js
+++ src/components/SpaceList.js
@@ -1,4 +1,5 @@
 import { borderBottom } from "../darkStyles";
+import { useTheme } from "../MainContext";
 import style from "../styles/SpaceList.module.css";
 
 // 스페이스 리스트의 상위 컴포넌트
@@ -8,8 +9,12 @@ function SpaceList({ spaceName }) {
 
 // 누르면 스페이스 리스트로 연결할 버튼
 function SpaceListButton({ children }) {
+  // 다크 테마
+  const theme = useTheme();
+  const isDark = theme === "dark";
+  const titleStyle = isDark ? borderBottom : undefined;
   return (
-    <h1 style={borderBottom} className={style.spaceListButton}>
+    <h1 style={titleStyle} className={style.spaceListButton}>
       {children}
     </h1>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,16 @@
 import { borderBottom } from "../darkStyles";
+import { useTheme } from "../MainContext";
 import style from "../styles/TodoList.module.css";
 
 // 투두리스트 탭이 눌리면 호출될 상위 컴포넌트
 function TodoList({ todoItems = [] }) {
+  // 다크 테마
+  const theme = useTheme();
+  const isDark = theme === "dark";
+  const titleStyle = isDark ? borderBottom : undefined;
   return (
     <section className={style.container}>
-      <h2 style={borderBottom} className={style.title}>
+      <h2 style={titleStyle} className={style.title}>
         TodoList
       </h2>
       <ItemList items={todoItems} />
